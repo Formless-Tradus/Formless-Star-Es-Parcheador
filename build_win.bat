fbb1b6763d7882ddfc1ff4561ccf25a122888fc4
Larrea-Mon <julia.larrea.montoya@protonmail.com>
2026-03-12 18:49:55 +0100

    Limpiar todo un poco para que sea mas facil exportar en el futuro

--- build_win.bat
+++ build_win.bat
@@ -1,1 +1,3 @@
 pyinstaller --add-data="parche-exe.bsdiff;." --add-data="parche-rpg.bsdiff;." .\parche-bsdiff-formless-star.py -D -F
+copy "dist\parche-bsdiff-formless-star.exe" "parche-es-formless-star.exe"
+"C:\Program Files\7-Zip\7z.exe" a "parche-es-formless-star-1-x-y.7z" "parche-es-formless-star.exe" "README.md"
